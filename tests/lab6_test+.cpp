b9e2b28edef5a33ce5a42d02b5d4a6a07820b1a0
Tsar Belyash <fev1792@gmail.com>
2024-11-27 22:21:05 +0300

    Tests have been added. Let is start debugging the programme. A report is required.

---
+++ tests/lab6_test+.cpp
@@ -0,0 +1,10 @@
+//Вам действительно нужен этот файл, Вы что-нибудь тестируете?
+
+#include <gtest/gtest.h>
+#include "game.h"
+#include <fstream>
+#include <sstream>
+
+TEST(test_01, additional_test){
+    EXPECT_TRUE(true);
+}
